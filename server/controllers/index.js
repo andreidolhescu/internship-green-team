be47c5d934a63cf17098446c03efe23b3a1d5fd3
StrujanVasile <intern.vasile.strujan@assist.ro>
2018-08-17 16:49:48 +0300

    Chapter Controller (Create, get, list, update, delete)4/6

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,8 +1,10 @@
 const testController = require('./testController');
 const users = require('./user');
+const chapters=require('./chapterController');
 // export all your controllers here
 
 module.exports = {
     testController,
-    users
+    users,
+    chapters
 };
